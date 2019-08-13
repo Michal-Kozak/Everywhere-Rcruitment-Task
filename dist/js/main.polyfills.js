cb43bc0d083f654d5176484514afc773b774c4b7
Michał <michalkozak176@gmail.com>
2019-08-13 02:03:12 +0200

    Add - Header basic styles

---
+++ dist/js/main.polyfills.js
@@ -0,0 +1,15 @@
+/*!
+ * Everywhere Rcruitment Task v0.0.1
+ * Rcruitment Task
+ * (c) 2019 Michal Kozak
+ * MIT License
+ * http://link-to-your-git-repo.com
+ */
+
+/**
+ * Element.matches() polyfill (simple version)
+ * https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
+ */
+if (!Element.prototype.matches) {
+	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
