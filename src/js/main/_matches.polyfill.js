b1c15d368ae3f6fd1602283a9e218f6c1000577a
Michał <michalkozak176@gmail.com>
2019-08-14 15:43:24 +0200

    Fix - Nav styles fix

--- src/js/main/_matches.polyfill.js
+++ src/js/main/_matches.polyfill.js
@@ -5,3 +5,26 @@
 if (!Element.prototype.matches) {
 	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
 }
+const navbar = document.querySelector('.navbar')
+
+navbar.querySelector('.toggle').addEventListener('click',()=>{
+
+	navbar.classList.toggle('collapsed')
+
+})
+
+
+
+window.addEventListener('scroll',(e)=>{
+
+	let windowY = window.pageYOffset
+
+	let navbarHeight = document.querySelector('.navbar').offsetHeight
+
+	if(windowY>navbarHeight) navbar.classList.add('sticky')
+else navbar.classList.remove('sticky')
+
+
+
+
+})
